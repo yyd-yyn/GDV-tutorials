1b2421c89c795fecbc6791a690a4ee7fa7545fd3
SpeziFisch-DE <fabian@mieg.de>
2023-08-17 18:05:58 +0200

    changed to classify

--- tutorials/src/22-img-classification-training.py
+++ tutorials/src/22-img-classification-training.py
@@ -4,6 +4,7 @@ import requests
 import html
 from ultralytics import YOLO  # pip install ultralytics needed
 from roboflow import Roboflow
+import os
 
 
 # TODO Implement image retrieval from web
@@ -21,12 +22,15 @@ def grab_image(query, show_image, save_image=False, save_path="tutorials/data/my
 
 
 # TODO retrieve multiple images
-def grab_more_images(query, image_count, save_path="tutorials/data/myimages/"):
-    for i in range(image_count):
-        grab_image('/1280x720/?' + query, False, True, save_path, "{0}_{1}.jpg".format(query, i))
+def grab_more_images(query, image_count, save_path="tutorials/data/myimages/", test_percentage=0.2):
+    for i in range((int)(image_count * test_percentage)):
+        grab_image('/1280x720/?' + query, False, True, save_path + "test/", "{0}_{1}.jpg".format(query, i))
+    for i in range((int)(image_count * (1 - test_percentage))):
+        grab_image('/1280x720/?' + query, False, True, save_path + "train/", "{0}_{1}.jpg".format(query, i))
 
 
 # TODO retrieve images for training
+# create folder structure as shown here: https://docs.ultralytics.com/datasets/classify/ with "train" and "test folder
 def grab_training_images(image_types, image_count, save_path="tutorials/data/myimages/"):
     for image_type in image_types:
         grab_more_images(image_type, image_count, save_path + image_type + "/")
@@ -35,7 +39,7 @@ def grab_training_images(image_types, image_count, save_path="tutorials/data/myi
 # images = grab_training_images(["bird", "forest"], 20)
 
 # TODO follow roboflow tutorial (https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data - Chapter 1.2 & 1.3)
-# to create dataset
+# to create dataset and download it
 
 
 def download_dataset(key, workspace, project_name, version, format):
@@ -47,7 +51,7 @@ def download_dataset(key, workspace, project_name, version, format):
 # TODO train model
 # base model needs to be downloaded first
 def train_model(data_path, _epochs):
-    model = YOLO("./tutorials/data/models/yolov5su.pt")
+    model = YOLO("./tutorials/data/models/yolov8n-cls.pt")
     results = model.train(data=data_path, epochs=_epochs)
     return results
 
@@ -65,23 +69,7 @@ def test_model(model_path):
     results = model.predict(source=img, stream=True, show=False)  # detect objects in img
     # parse results and draw bounding boxes on img
     for result in results:
-        boxes = result.boxes
-        class_names = result.names
-        # draw bounding boxes on img
-        for i in range(len(boxes)):
-            box = boxes[i]
-            # print(box) # print box object to find out which information is available
-            # draw bounding box
-            x1 = (int)(box.xyxy.numpy()[0][0])
-            y1 = (int)(box.xyxy.numpy()[0][1])
-            x2 = (int)(box.xyxy.numpy()[0][2])
-            y2 = (int)(box.xyxy.numpy()[0][3])
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            # draw class name and score
-            class_name = class_names[(int)(box.cls.numpy()[0])]
-            cv2.putText(img, class_name, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            score = box.conf.numpy()[0]
-            cv2.putText(img, str(score), (x1, y1 + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        print(result.names[0] + ": " + str(result.probs.data[0].item()))
     # show image with bounding boxes
     cv2.imshow("result", img)
     return results
@@ -94,9 +82,8 @@ while True:
     # present options
     print("press q to quit")
     print("press 1 to grab images")
-    print("press 2 to download dataset")
-    print("press 3 to train model")
-    print("press 4 to test model")
+    print("press 2 to train model")
+    print("press 3 to test model")
     cv2.namedWindow("result", cv2.WINDOW_NORMAL)
     key = cv2.waitKey(0)
     # press q to close the window
@@ -110,25 +97,14 @@ while True:
         # set save path
         save_path = "tutorials/data/myimages/"
         grab_training_images(image_descriptors, image_count, save_path)
-        break
-    # press 2 to download dataset
+    # press 2 to train model
     if key == ord('2'):
-        key = input("input your roboflow api key: ")
-        workspace = input("input your roboflow workspace: ")
-        project_name = input("input your roboflow project name: ")
-        version = (int)(input("input the version of your roboflow project: "))
-        format = input("input the format of your roboflow project: ")
-        download_dataset(key, workspace, project_name, version, format)
-        break
-    # press 3 to train model
+        data_path = './tutorials/data/myimages'
+        _epochs = (int)(input("input the number of epochs: "))
+        train_model(data_path, _epochs)
+    # press 3 to test model
     if key == ord('3'):
-        data_path = "./tutorials/data/bird-or-forest-2/data.yaml"
-        epochs = (int)(input("input the number of epochs: "))
-        train_model(data_path, epochs)
-        break
-    # press 4 to test model
-    if key == ord('4'):
-        model_path = "./tutorials/data/runs/detect/train2/weights/best.pt"
+        model_path = "./tutorials/data/runs/classify/train3/weights/best.pt"
         while True:
             test_model(model_path)
             if cv2.waitKey(0) == ord('q'):
